9b1205f15730368c33542678055109b5b2fa5ef0
Alexis Morales <alx.morales@hadoukendev.com>
2017-05-15 10:23:38 -0500

    Sitio Fitcoach

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,20 @@
+@extends('plantilla')
+
+@section('pagecontent')
+	<section class="container">
+		<div class="homeBxSliderWrap">
+			<div class="homeBxSlider">
+
+
+                <div class="slide" data-slide="0" style="background-image: url(./images/content/BG-CLASES.jpg);">
+
+                    <div class="slideDesc">
+                   <h3 style="color: #">clases a domicilio <br><a class="learnMore" href="#D58628">PROXIMAMENTE</a></h3>
+						</div>
+				</div>
+
+				</div>
+
+			</div>
+		</div>
+@endsection
