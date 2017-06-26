55f89d43e37318be723312a506409daa4ba05f41
Alexis Morales <alx.morales@hadoukendev.com>
2017-06-26 14:53:04 -0500

    Avance producto

--- resources/views/producto.blade.php
+++ resources/views/producto.blade.php
@@ -11,35 +11,45 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-6">
-				<h1>Clase</h1>
+			<div class="col-sm-6 col-md-4 col-md-offset-2">
+				<div class="product_large">
+					<h1>Clase</h1>
 
-				<small>Precio</small>
-				<h3>$500</h3>
-				<form action="{{ url('/carrito') }}" method="post">
-{!! csrf_field() !!}
-				<input type="hidden" name="clase" value="1">
 
-				<strong>Elije una fecha</strong>
-				<div class="form-group">
+					<h3 class="precio">$500 MXN</h3>
+					<form action="{{ url('/carrito') }}" method="post">
+						{!! csrf_field() !!}
+						<input type="hidden" name="clase" value="1">
 
-					<div class="col-sm-9">
-						<div class="input-group">
-							<input class="form-control datepicker" type="text"  name="fecha" required><span class="input-group-addon"><i class="fa fa-calendar" aria-hidden="true"></i></span>
+						<strong>Elije una fecha</strong>
+						<div class="form-group">
+								<div class="input-group">
+									<input class="form-control datepicker" type="text"  name="fecha" required><span class="input-group-addon"><i class="fa fa-calendar" aria-hidden="true"></i></span>
+								</div>
 						</div>
-					</div>
+						<br>
+						<strong>Elije tu horario y coach</strong>
+						<select class="form-control" name="horario">
+							<option value="">selecciona</option>
+							<option value="1">{{$horario->hora}} - {{$horario->user->name}} - 4.6 ★ </option>
+						</select>
+						<br>
+						<strong>Elije tu dirección</strong>
+						<select class="form-control" name="direccion">
+							<option value="">selecciona</option>
+							@foreach ($direcciones as $direccion)
+								<option value="{{$direccion->id}}">{{$direccion->identificador}}</option>
+							@endforeach
+
+						</select>
+						<p>&nbsp;</p>
+						<a href="#" class="btn btn-primary pull-right" >Agregar al carrito</a>
+						<p>&nbsp;</p>
+					</form>
 				</div>
 
-				<br>
-				<strong>Elije tu horario y coach</strong>
-				<select class="form-control" name="horario">
-					<option value="">selecciona</option>
-					<option value="1">8:00 - Coach 1 - 4.6 ★ </option>
-				</select>
-				<button  type="submit"class="btn btn-primary" >Agregar al carrito</button>
-			</form>
 			</div>
-			<div class="col-md-6">
+			<div class="col-sm-6">
 
 			</div>
 		</div>
